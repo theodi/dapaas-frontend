72860b22f1b05c2ce80bd047bb3c0e3f0c7beb08
pezholio <pezholio@gmail.com>
2013-12-06 10:03:43 +0000

    Bare Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DapaasFrontend::Application.config.secret_key_base = 'c8d865601adcbcdcb025e604917043a5444fd3e1903ecd32067a61181472e0c5eeb3af72f754cc5f24917d356b67c6bb5b27d30f62195205feb5c8bba410ca5e'
