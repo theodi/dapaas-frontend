c0dad65787a0d48762590e2a2350c326333c048d
pezholio <pezholio@gmail.com>
2013-12-09 10:46:02 +0000

    Pull in news content

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,4 +4,23 @@ class RootController < ApplicationController
   def index
   end
   
+  def news_list
+    @section = "news"
+    results = content_api.with_tag("news")
+    @artefacts = results.results
+    @title = "News"
+    respond_to do |format|
+      format.html do
+        render "list/news"
+      end
+      format.json do
+        redirect_to "#{api_domain}/with_tag.json?tag=#{params[:section].singularize}&role=dapaas"
+      end
+      format.atom do
+        slimmer_template nil
+        render "list/feed"
+      end
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,19 @@ DapaasFrontend::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'root#index'
 
+  [:blog, :news, :events].each do |section|
+    section_slug = section.to_s.dasherize
+    get "#{section_slug}", as: "#{section}_section", to: "root##{section}_list", :section => section_slug
+
+    get "#{section_slug}/module", as: "#{section}_list_module", to: "root##{section}_list_module", :section => section_slug
+
+    get "#{section_slug}/:slug", as: "#{section}_article", to: "root##{section}_article", :section => section_slug
+
+    get "#{section_slug}/:slug/module", as: "#{section}_module", to: "root##{section}_module", :section => section_slug
+
+    get "#{section_slug}/:slug/badge", as: "#{section}_badge", to: 'root#badge', :section => section_slug
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
