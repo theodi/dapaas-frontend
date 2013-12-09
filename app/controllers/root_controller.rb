1db79d1b36c389c95f9c78012a5f2b594809a855
pezholio <pezholio@gmail.com>
2013-12-09 12:02:14 +0000

    Metaprogram all the things

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,29 +3,22 @@ require 'artefact_retriever'
 class RootController < ApplicationController
   slimmer_template :dapaas
   
-  def index
-  end
-  
-  def news_list
-    @section = "news"
-    results = content_api.with_tag("news")
-    @artefacts = results.results
-    @title = "News"
-    respond_to do |format|
-      format.html do
-        render "list/news"
-      end
-      format.json do
-        redirect_to "#{api_domain}/with_tag.json?tag=#{params[:section].singularize}&role=dapaas"
-      end
-      format.atom do
-        slimmer_template nil
-        render "list/feed"
-      end
+  def action_missing(name, *args, &block)
+    if name.to_s =~ /^(.*)_list$/
+      list(params)
+    elsif name.to_s =~ /^(.*)_article$/
+      article(params)
+    else
+      super
     end
   end
   
-  def news_article
+  def index
+  end
+  
+  private
+  
+  def article(params)
     artefact = ArtefactRetriever.new(content_api, Rails.logger, statsd).
                   fetch_artefact(params[:slug], params[:edition], nil, nil)
 
@@ -41,6 +34,29 @@ class RootController < ApplicationController
       end
       format.json do
         redirect_to "#{api_domain}/#{params[:slug]}.json"
+      end
+    end
+  end
+  
+  def list(params)
+    @section = params[:section].parameterize
+    @artefacts = content_api.with_tag(params[:section].singularize).results
+    @title = params[:section].gsub('-', ' ').humanize.capitalize
+    respond_to do |format|
+      format.html do
+        begin
+          # Use a specific template if present
+          render "list/#{params[:section]}"
+        rescue
+          render "list/list"
+        end
+      end
+      format.json do
+        redirect_to "#{api_domain}/with_tag.json?tag=#{params[:section].singularize}&role=dapaas"
+      end
+      format.atom do
+        slimmer_template nil
+        render "list/feed"
       end
     end
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def module_block(section, publication)
+  	begin
+  		render :partial => "module/#{section.gsub('-', '_')}", :locals => { :section => section, :publication => publication }
+    rescue ActionView::MissingTemplate
+  	  render :partial => 'module/block', :locals => { :section => section, :publication => publication }
+  	end
+  end
+  
 end
