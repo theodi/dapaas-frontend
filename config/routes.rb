3c0a243d648cc0e4f90882e2c5291897c1c83bfd
pezholio <pezholio@gmail.com>
2013-12-16 13:01:07 +0000

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ DapaasFrontend::Application.routes.draw do
 
   [:blog, :news, :events, :partners].each do |section|
     section_slug = section.to_s.dasherize
-    get "#{section_slug}", as: "#{section}_section", to: "root##{section}_list", :section => section_slug
+    get "dapaas-#{section_slug}", as: "#{section}_section", to: "root##{section}_list", :section => section_slug
 
-    get "#{section_slug}/module", as: "#{section}_list_module", to: "root##{section}_list_module", :section => section_slug
+    get "dapaas-#{section_slug}/module", as: "#{section}_list_module", to: "root##{section}_list_module", :section => section_slug
 
-    get "#{section_slug}/:slug", as: "#{section}_article", to: "root##{section}_article", :section => section_slug
+    get "dapaas-#{section_slug}/:slug", as: "#{section}_article", to: "root##{section}_article", :section => section_slug
 
-    get "#{section_slug}/:slug/module", as: "#{section}_module", to: "root##{section}_module", :section => section_slug
+    get "dapaas-#{section_slug}/:slug/module", as: "#{section}_module", to: "root##{section}_module", :section => section_slug
 
-    get "#{section_slug}/:slug/badge", as: "#{section}_badge", to: 'root#badge', :section => section_slug
+    get "dapaas-#{section_slug}/:slug/badge", as: "#{section}_badge", to: 'root#badge', :section => section_slug
   end
   
   get ":slug", as: "page", to: 'root#page'
