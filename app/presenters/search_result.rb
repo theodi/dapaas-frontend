867881a316480a7b64608c8073e91c20920c318a
pezholio <pezholio@gmail.com>
2014-05-01 15:02:25 +0100

    Add accessor to search result

--- app/presenters/search_result.rb
+++ app/presenters/search_result.rb
@@ -4,4 +4,14 @@ class SearchResult
     @result = result.stringify_keys!
   end
 
+  def self.result_accessor(*keys)
+    keys.each do |key|
+      define_method key do
+        @result[key.to_s]
+      end
+    end
+  end
+
+  result_accessor :link, :title, :description, :format, :es_score
+
 end
