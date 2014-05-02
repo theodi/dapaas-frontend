8ccdf2f0a78662a5fb1f42f7cf652162ae97a2d5
pezholio <pezholio@gmail.com>
2014-05-02 12:55:08 +0100

    Get stuff from details hash too

--- app/presenters/search_result.rb
+++ app/presenters/search_result.rb
@@ -12,6 +12,15 @@ class SearchResult
     end
   end
 
-  result_accessor :link, :title, :description, :format, :es_score
+  def self.details_accessor(*keys)
+    keys.each do |key|
+      define_method key do
+        @result['details'][key.to_s]
+      end
+    end
+  end
+
+  result_accessor :title, :es_score
+  details_accessor :slug, :format, :description
 
 end
